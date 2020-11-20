ce57cb1c4053bac9f28440a5f64462a36051049e
posterrieri <adamdorucu@gmail.com>
2020-11-20 16:04:18 +0300

    Logit tests

---
+++ mllib/supervised/__init__.py
@@ -0,0 +1,5 @@
+from .parametric import LinearRegression
+from .parametric import LogisticRegressionClassifier
+
+__all__ = ['LinearRegression',
+            'LogisticRegressionClassifier']
--- mllib/supervised/parametric.py
+++ mllib/supervised/parametric.py
@@ -3,17 +3,17 @@ import numpy as np
 
 class LinearRegression:
     """Linear regression algorithm"""
-    def fit(self, X: np.array, y: np.array, lamb=0, add_intercept=True) -> None:
+    def fit(self, X, y, lamb=0, add_intercept=True):
         """Fits the training data using normal equation"""
         if add_intercept:
-            X = np.column_stack((np.ones((X.shape[0], 1), dtype=X.type)))
+            X = np.column_stack((np.ones((X.shape[0], 1), dtype=X.dtype), X))
         n, p = X.shape
-        self.Theta = np.inv(X.T @ X + n * lamb * np.eye(p+1)) @ X.T @ y
+        self.Theta = np.linalg.inv(X.T @ X) @ X.T @ y
 
     def predict(self, X, add_intercept=True):
         """Makes predictions on the given data"""
         if add_intercept:
-            X = np.column_stack((np.ones((X.shape[0], 1), dtype=X.type)))
+            X = np.linalg.inv(X.T @ X) @ X.T @ y
         return X @ self.Theta
 
 
@@ -24,15 +24,18 @@ class LogisticRegressionClassifier:
 
     def fit(self, X, y, iters=200, lr=0.01):
         """Fits the training data"""
-        self.X, self.y = X, y
+        self.y = y
+        self.X = np.column_stack((np.ones((X.shape[0], 1), dtype=X.dtype), X))
         self.m, self.n = X.shape
         self.Theta = np.random.randn(self.n+1, 1)
-        loss_prime = lambda x, y, theta: 1/self.m * (self._sigmoid(x * theta) - y).T * x
+        print(self.X, self.y, self.Theta)
+        loss_prime = lambda x, y, theta: 1/self.m * (self._sigmoid(x @ theta) - y).T @ x
         self._gradient_descent(iters=iters, loss_prime=loss_prime, lr=lr)
 
     def predict(self, X):
         """Makes prediction"""
-        return 1 if self._sigmoid(X * self.Theta) > self.r else -1
+        X = np.column_stack((np.ones((X.shape[0], 1), dtype=X.dtype), X))
+        return np.array([[1 if res > self.r else -1] for res in self._sigmoid(X @ self.Theta)])
 
     def _sigmoid(self, Z):
         """Sigmoid function"""
@@ -42,4 +45,4 @@ class LogisticRegressionClassifier:
         """Gradient descent algorithm"""
         for i in range(iters):
             grad = loss_prime(self.X, self.y, self.Theta)
-            self.Theta -= lr * grad
+            self.Theta -= lr * grad.T
